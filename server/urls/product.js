1449ff2977ae0d2e7b2a49cebd93edd938505cf6
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-04-20 06:27:08 +0100

    Update product.js
    permissions update

--- server/urls/product.js
+++ server/urls/product.js
@@ -5,11 +5,11 @@ import {token ,auth, authAdmin} from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/',  auth, createProduct);
-router.put('/:id',  auth, updateProduct);
-router.delete('/:id',  auth, deleteProduct);
-router.get('/search/:id',  auth, getProduct);
-router.get('/',  auth, getAllProducts);
+router.post('/',  authAdmin, createProduct);
+router.put('/:id',  authAdmin, updateProduct);
+router.delete('/:id',  authAdmin, deleteProduct);
+router.get('/search/:id', getProduct);
+router.get('/', getAllProducts);
 router.get('/stats',  authAdmin, getProductsStats);
 
 export default router;
