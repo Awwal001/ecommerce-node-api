42b3b39f3e0f7f44e6ff091453c465f7cce23a7b
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-05 10:03:55 +0100

    Cleaning codes

--- server/urls/auth.js
+++ server/urls/auth.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { signin, signup } from '../views/auth.js';
-
+import {
+    signin,
+    signup,
+    logout,
+    forgotPassword,
+    resetPassword,
+    updatePassword,
+  } from '../views/auth.js';
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/logout", logout);
 router.post('/login', signin);
 router.post('/register', signup);
+router.put("/me/update", auth, updatePassword);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
 export default router;
