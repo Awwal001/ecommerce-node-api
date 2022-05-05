94339fbc875ebd524801a75673fe0fee28efbd9d
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-05 10:24:03 +0100

    Cleaning codes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import fileUpload from 'express-fileupload';
 import userRoutes from './urls/user.js';
 import authRoutes from './urls/auth.js';
 import cartRoutes from './urls/cart.js';
@@ -11,7 +15,19 @@ import productRoutes from './urls/product.js';
 const app = express();
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
+app.use(fileUpload());
+
+
+
 app.use('/api/user', userRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/auth', authRoutes);
