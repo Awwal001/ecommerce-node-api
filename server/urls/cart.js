3715fd99955e8557548c52f9e1e162eb4dd8c475
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-05 10:08:03 +0100

    Cleaning codes

--- server/urls/cart.js
+++ server/urls/cart.js
@@ -1,15 +1,14 @@
 import express from "express";
 import { createCart, updateCart, deleteCart, getUserCart, getAllCarts } from '../views/cart.js';
 
-import {token ,auth, authAdmin} from "../middleware/auth.js";
+import {Token ,auth, authAdmin} from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post('/',  Token, createCart);
-router.put('/:id',  auth, updateCart);
-router.delete('/:id',  deleteCart);
+router.put('/:id', auth,  updateCart);
+router.delete('/:id', auth, deleteCart);
 router.get('/search/:userId',  auth, getUserCart);
 router.get('/', authAdmin, getAllCarts);
-//router.get('/stats',  getCartsStats);
 
 export default router;
