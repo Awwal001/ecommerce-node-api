3a4ba1eb7ff3e44f167af7e11c89d5687b6580fb
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-05 09:48:54 +0100

    Cleaning codes

--- server/models/product.js
+++ server/models/product.js
@@ -2,16 +2,79 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        title: {type: String, required: true, unique: true},
-        description: {type: String, required: true},
-        img: {type: String, required: true},
-        categories: {type: Array},
-        size: {type: String},
-        color: {type: String},
-        creator: {type: String},
-        comments: {type: [String], default: []},
-        ratings: {type: [Number], default: []},
-        price: {type: Number, required: true},
+            name: {
+            type: String,
+            required: [true, "Please Enter product Name"],
+            trim: true,
+            },
+            description: {
+            type: String,
+            required: [true, "Please Enter product Description"],
+            },
+            price: {
+            type: Number,
+            required: [true, "Please Enter product Price"],
+            maxLength: [8, "Price cannot exceed 8 characters"],
+            },
+            ratings: {
+            type: Number,
+            default: 0,
+            },
+            images: [
+            {
+                public_id: {
+                type: String,
+                required: true,
+                },
+                url: {
+                type: String,
+                required: true,
+                },
+            },
+            ],
+            size: {type: Array},
+            color: {type: Array},
+            categories: {
+            type: Array,
+            required: [true, "Please Enter Product Category"],
+            },
+            Stock: {
+            type: Number,
+            required: [true, "Please Enter product Stock"],
+            maxLength: [4, "Stock cannot exceed 4 characters"],
+            default: 1,
+            },
+            numOfReviews: {
+            type: Number,
+            default: 0,
+            },
+            reviews: [
+            {
+                user: {
+                type: mongoose.Schema.ObjectId,
+                ref: "user",
+                required: true,
+                },
+                name: {
+                type: String,
+                required: true,
+                },
+                rating: {
+                type: Number,
+                required: true,
+                },
+                comment: {
+                type: String,
+                required: true,
+                },
+            },
+            ],
+        
+            user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "user",
+            required: true,
+            },
     },
     {timestamps: true}
 );
