1d8ce08db2bb45da4005e5d92e5449eec15efd3e
Awwal001 <75147024+Awwal001@users.noreply.github.com>
2022-05-05 10:10:58 +0100

    Cleaning codes

--- server/urls/product.js
+++ server/urls/product.js
@@ -1,15 +1,30 @@
 import express from "express";
-import { createProduct, updateProduct, deleteProduct, getProduct, getAllProducts, getProductsStats } from '../views/product.js';
+import { 
+    createProduct, 
+    updateProduct, 
+    deleteProduct, 
+    getProduct, 
+    getAllProducts, 
+    getProductsStats,
+    createProductReview,
+    getSingleProductReviews,
+    deleteReview 
+} from '../views/product.js';
 
-import {token ,auth, authAdmin} from "../middleware/auth.js";
+import {auth, authAdmin} from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post('/',  authAdmin, createProduct);
-router.put('/:id',  authAdmin, updateProduct);
-router.delete('/:id',  authAdmin, deleteProduct);
+router.post('/', authAdmin, createProduct);
+router.put('/:id', authAdmin, updateProduct);
+router.delete('/:id', authAdmin, deleteProduct);
 router.get('/search/:id', getProduct);
 router.get('/', getAllProducts);
-router.get('/stats',  authAdmin, getProductsStats);
+router.get('/stats', authAdmin,  getProductsStats);
+
+
+router.post('/', auth, createProductReview);
+router.get('/:id', auth, getSingleProductReviews);
+router.delete('/:id', auth, deleteReview);
 
 export default router;
